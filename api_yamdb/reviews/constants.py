086cb1a23cfebffa6df2a3fe6a78ea1db6d4c324
Илья Росманов <rosmanov2012@yandex.ru>
2023-07-25 13:28:09 +0300

    v.1

---
+++ api_yamdb/reviews/constants.py
@@ -0,0 +1,5 @@
+TITLE_STRING_LENGTH = 50  # Длина названия произведения
+CATEGORY_STRING_LENGTH = 50  # Длина названия категории
+GENRE_STRING_LENGTH = 50  # Длина названия жанра
+COMMENT_STRING_LENGTH = 30  # Длина текста комментария
+REVIEW_STRING_LENGTH = 30  # Длина текста отзыва
